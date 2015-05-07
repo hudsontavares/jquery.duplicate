eebc6513c504b94c75945fddd4b0517cd224b792
hudsontavares <hudson.tavares@gmail.com>
2015-05-07 16:48:28 -0300

    JS comments, default language is english, refactor
    The add button is now firstly defined and then added to the
    DOM tree. It prevents that a mistakenly written className for
    the add button may result in a button that doesn`t works.

--- jquery.duplicate.js
+++ jquery.duplicate.js
@@ -1,27 +1,36 @@
-/* @hudson_ */
-
+/**
+ * A plugin that allows you define reproducable fields
+ * following it's patterns.
+*/
 (
     function($)
     {
         $.fn.duplicate = function(params)
         {
+            /* The basic settings of our duplicable content; they can all be defined on duplicate() call */
             var settings = $.extend
             (
                 true,
-                {
+                { 
+                    /* The class that must be defined on the clonable element */
                     duplicableClass     : 'clonable',
+                    /* The data field for each DOM entry that stores the unique identifier of the related registry  */
                     registryIdField     : 'data-id',
+                    /* The data field for each DOM entry that stores the class name of the related registry  */
                     registryClassName   : 'data-class-name',
+                    /* If required, you can specify the maximum number of entries allowedd */
                     maxItems            : 0,
+                    /* The add button that will be automatically appended to the DOM tree, adjacent to the duplicable container */
                     addButton           :
                     {
-                        text        : 'Adicionar',
+                        text        : 'Add',
                         className   : 'button duplicate add',
                         onCreate    : function(element){return element;}
                     },
+                    /* The remove button that will be automatically appended to the DOM tree, adjacent to the duplicable container */
                     removeButton        :
                     {
-                        text            : 'Remover',
+                        text            : 'Remove',
                         className       : 'button duplicate remove',
                         onRemoveStart   : function(element){return true;},
                         onRemoveEnd     : function(id, message, element){return true;}
@@ -30,6 +39,10 @@
                 params
             );
             
+            /*
+                The event is binded on the holder itself and uses bubbling to keep the memory footprint
+                low, even with huge lists of duplicate items
+            */
             $(this).bind
             (
                 'click',
@@ -68,16 +81,13 @@
                 }
             );
             
+            /* Returns the current object for chaining purposes */
             return this.each
             (
                 function(index)
                 {
-                    var source     = $(this).find('li.' + settings.duplicableClass).removeClass('clonable').addClass('registry'), target = $(this);
-                    this.basicHtml = source[0].outerHTML;
-                    source.remove();
-                    
-                    /* Botão "adicionar item" */
-                    $(this).after
+                    var source  = $(this).find('li.' + settings.duplicableClass).removeClass('clonable').addClass('registry'), target = $(this),
+                    add     = $
                     (
                         [
                             '<button type="button" class="',
@@ -87,8 +97,19 @@
                             '</button>'
                         ].join('')
                     );
+
+                    /* The basic HTML is a copy of our duplicable element; we copy it and remove! */
+                    this.basicHtml = source[0].outerHTML;
+                    source.remove();
                     
-                    $('button.' + settings.addButton.className.split(' ').join('.'), this.parentNode).bind
+                    /* The add button must enter the tree */
+                    $(this).after(add);
+                    
+                    /*
+                        The add item button behavior; as it is located outside the duplicable tree,
+                        we must listen to it here
+                    */
+                    add.bind
                     (
                         'click',
                         function(e)
